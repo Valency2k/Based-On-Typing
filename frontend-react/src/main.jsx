c541e3b92eaae1867a39c368d2dffa5ac28a36d4
Valency2k <aronejoshua@gmail.com>
2025-11-28 12:02:21 +0100

    fix: resolve missing @wagmi/core dependency and buffer polyfills

--- frontend-react/src/main.jsx
+++ frontend-react/src/main.jsx
@@ -2,9 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import { Buffer } from 'buffer'
-
-globalThis.Buffer = Buffer
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { WagmiProvider } from 'wagmi'
